328270a9c54772ba332807da37cf51fcf4f49c7a
reck-coder123 <105230070+reck-coder123@users.noreply.github.com>
2022-08-21 19:49:25 +0530

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,41 +1,33 @@
-CREATE DATABASE IF NOT EXISTS `hirelogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `hirelogin`;
-CREATE TABLE IF NOT EXISTS `Job`(
+CREATE DATABASE IF NOT EXISTS `hiretlogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `hiretlogin`;
+CREATE TABLE IF NOT EXISTS `Jobs`(
 `id` int(65) NOT NULL auto_increment,
 `nameA` varchar(50) NOT NULL,
-`usernameA` varchar(50) NOT NULL,
+`username` varchar(50) NOT NULL,
 `passwordA` varchar(50) NOT NULL,
 `emailA` varchar(50) NOT NULL,
 PRIMARY KEY (`id`) )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `New`(
+CREATE TABLE IF NOT EXISTS `News`(
 `id` int(65) NOT NULL auto_increment,
 `nameJ` varchar(50) NOT NULL,
-`usernameJ` varchar(50) NOT NULL,
+`username` varchar(50) NOT NULL,
 `passwordJ` varchar(50) NOT NULL,
 `emailJ` varchar(50) NOT NULL,
 PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `info`(
+CREATE TABLE IF NOT EXISTS `infos`(
 `id` int(65) NOT NULL auto_increment,
 `job` varchar(50) NOT NULL,
+`description` varchar(5000) NOT NULL,
 PRIMARY KEY (`id`))ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-use hirelogin;
-alter table info add column description varchar(500) not null; 
-
-
-
-CREATE TABLE IF NOT EXISTS `choose` (
+CREATE TABLE IF NOT EXISTS `chose` (
 `id` int(65) NOT NULL auto_increment,
 `Name` varchar(50) NOT NULL,
 `jprofile` varchar(100) NOT NULL ,
 PRIMARY KEY (`id`)
 
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-use hirelogin;
-
-show tables;
-describe info;
+select * from Jobs;
